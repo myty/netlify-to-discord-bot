9c5e8ba083dad5a409227ddbd979b64afcef4420
Michael Tyson <michaeltyson@outlook.com>
2022-01-26 23:25:59 -0500

    update jimmy version

--- deps.ts
+++ deps.ts
@@ -1,5 +1,2 @@
 export { Application, Router } from "https://deno.land/x/oak/mod.ts";
-export {
-  Mediator,
-  Request,
-} from "https://deno.land/x/jimmy@v0.1.0-preview2/mod.ts";
+export { Mediator, Request } from "https://deno.land/x/jimmy@v0.2.1/mod.ts";
